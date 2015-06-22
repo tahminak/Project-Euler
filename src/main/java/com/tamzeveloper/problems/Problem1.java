a93f7abe36a320a2b10d94945f7a1a06bd92df70
Tahmina Khan <tahmina.khan@gmail.com>
2015-06-22 08:21:15 -0400

    Add problem 3

--- src/main/java/com/tamzeveloper/problems/Problem1.java
+++ src/main/java/com/tamzeveloper/problems/Problem1.java
@@ -1,4 +1,13 @@
 package com.tamzeveloper.problems;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Created by tahmina on 15-05-29.
  */
@@ -15,14 +24,13 @@ Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
 
-    public static int getSumOfMultiples3and5(int num){
+    public  int getSumOfMultiples3and5(int num){
 
 
         int sum=0;
 
 
         for (int i=1;i<num;i++){
-
             if(i%3==0||i%5==0){
                 sum+=i;
             }
@@ -35,5 +43,17 @@ Find the sum of all the multiples of 3 or 5 below 1000.
     }
 
 
+    @Test(timeout=50)
+    public void TestSumOfMultiples3and5(){
 
+        int sumOfMultiples = getSumOfMultiples3and5(1_000);
+
+        System.out.println(getSumOfMultiples3and5(1000));
+        Assert.assertEquals("Incorrect sum of multiples of 3 or 5 below 1,000", 233168, getSumOfMultiples3and5(1000));
+
+        assertThat("Incorrect sum of multiples of 3 or 5 below 1,000",sumOfMultiples, is(233168));
+        Assert.assertEquals( "Incorrect sum of multiples of 3 or 5 below 10,000",23331668, getSumOfMultiples3and5(10000));
+
+
+    }
 }
--- src/test/java/Problem1Test.java
+++ src/test/java/Problem1Test.java
@@ -1,5 +1,3 @@
-import com.tamzeveloper.problems.Problem1;
-import org.junit.Assert;
 import org.junit.Test;
 /**
  * Created by tahmina on 15-05-29.
@@ -9,12 +7,9 @@ public class Problem1Test {
 
     @Test(timeout=50)
     public void TestSumOfMultiples3and5(){
-
-
-
-        Assert.assertEquals( "Incorrect sum of multiples of 3 or 5 below 1,000",233168, Problem1.getSumOfMultiples3and5(1000));
-
-        Assert.assertEquals( "Incorrect sum of multiples of 3 or 5 below 10,000",23331668, Problem1.getSumOfMultiples3and5(10000));
+//        Assert.assertEquals( "Incorrect sum of multiples of 3 or 5 below 1,000",233168, Problem1.getSumOfMultiples3and5(1000));
+//
+//        Assert.assertEquals( "Incorrect sum of multiples of 3 or 5 below 10,000",23331668, Problem1.getSumOfMultiples3and5(10000));
 
 
     }
