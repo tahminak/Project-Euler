3196fbc0762c7c25529a4359fb2f040dff3e5258
Tahmina Khan <tahmina.khan@gmail.com>
2015-06-28 11:39:05 -0400

    Add problem 4

--- src/main/java/com/tamzeveloper/problems/Problem4.java
+++ src/main/java/com/tamzeveloper/problems/Problem4.java
@@ -20,11 +20,21 @@ public class Problem4 {
 
 
     @Test(timeout=80)
-    public void TestLargestPalindrom(){
+    public void TestLargestPalindromOf_3Digits(){
 
          int largestPalindrom=largestPalindromNum(3);
          System.out.println("\nLargest palindrom of 2- digits = "+largestPalindrom);
          assertThat("Incorrect Largest Palindrom of 2-digit numbers",largestPalindrom, is(906609));
+
+
+    }
+
+    @Test(timeout=80)
+    public void TestLargestPalindromOf_2Digits(){
+
+        int largestPalindrom=largestPalindromNum(2);
+        System.out.println("\nLargest palindrom of 2- digits = "+largestPalindrom);
+        assertThat("Incorrect Largest Palindrom of 2-digit numbers",largestPalindrom, is(9009));
 
 
     }
@@ -37,9 +47,7 @@ public class Problem4 {
         assertTrue("Incorrect Palindrom number", isPalindrome(9009 + ""));
 
         assertFalse("Incorrect Palindrom number", isPalindrome(9109 + ""));
-        //  assertTrue("Incorrect Palindrom number", isPalindrome(9009 + ""));
 
-       //assertFalse("Incorrect Palindrom number", isPalindrome(Integer.toString(2200022)));
     }
 
 
@@ -48,6 +56,9 @@ public class Problem4 {
         //9 or 99 or 999
         int largestNumberOfNum;
 
+        // 1 or 10 or 100
+        int smalledNumOfNum;
+
         String nDigitsStr="";
 
         for(int i=1;i<=num;i++){
@@ -55,12 +66,22 @@ public class Problem4 {
         }
 
       largestNumberOfNum=Integer.valueOf(nDigitsStr);
-      System.out.println(largestNumberOfNum);
+
+       //create smallest number
+       nDigitsStr="1";
+
+       for(int i=1;i<num;i++){
+           nDigitsStr+="0";
+       }
+
+      smalledNumOfNum=Integer.valueOf(nDigitsStr);
+
+      //System.out.println(largestNumberOfNum+" "+smalledNumOfNum);
 
         int x, y, product, max = 0;
 
-        for (x = 100; x <= 999; x++) {
-            for (y = x; y <= 999; y++) {
+        for (x = smalledNumOfNum; x <= largestNumberOfNum; x++) {
+            for (y = x; y <= largestNumberOfNum; y++) {
                 product = x * y;
                 if (isPalindrome(Integer.toString(product))) {
                     if( max < product ) { // this is new
@@ -77,8 +98,10 @@ public class Problem4 {
 
     public boolean isPalindrome(String str) {
 
+
         StringBuilder reverese=new StringBuilder(str).reverse();
 
+        //Reverse of a palindrom string is equal to the originial one
         return str.equals(reverese.toString());
     }
 }
